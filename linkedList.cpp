e28b1557fbcaacab55da398f43c4bc8d94f8c308
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-10 08:37:35 -0500

    Continue working on LL.

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,13 +1,15 @@
+#include <iostream>
+
 class Node
 {
 public:
     int value;
     Node *next;
-    Node(int val)
+    Node(int value)
     {
-        this->value = val;
+        this->value = value;
         this->next = nullptr;
-    };
+    }
 };
 
 class LinkedList
@@ -20,9 +22,40 @@ private:
 public:
     LinkedList(int value)
     {
-        Node *node = new Node(value);
-        this->head = node;
-        this->tail = node;
+        Node *newNode = new Node(value);
+        this->head = newNode;
+        this->tail = newNode;
         this->length = 1;
-    };
+    }
+    void printList()
+    {
+        Node *temp = this->head;
+        while (temp)
+        {
+            std::cout << temp->value << ", " << std::endl;
+            temp = temp->next;
+        }
+    }
+    void getHead()
+    {
+        std::cout << this->head->value << std::endl;
+    }
+    void getTail()
+    {
+        std::cout << this->tail->value << std::endl;
+    }
+    void getLength()
+    {
+        std::cout << this->length << std::endl;
+    }
 };
+
+int main()
+{
+    LinkedList *myLinkedList = new LinkedList(1);
+    myLinkedList->printList();
+    myLinkedList->getHead();
+    myLinkedList->getTail();
+    myLinkedList->getLength();
+    return 0;
+}
