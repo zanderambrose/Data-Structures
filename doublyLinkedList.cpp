446d8b053de67ad8cba8bfae1e5f33c6f66b7e9c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-16 08:47:45 -0400

    Add helper methods for printing list, head and tail.

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -30,4 +30,21 @@ public:
         this->tail = firstNode;
         this->length = 1;
     }
+    void getHead()
+    {
+        std::cout << "Head: " << this->head->value << std::endl;
+    }
+    void getTail()
+    {
+        std::cout << "Tail: " << this->tail->value << std::endl;
+    }
+    void printList()
+    {
+        Node *current = this->head;
+        while (current)
+        {
+            std::cout << current->value << std::endl;
+            current = current->next;
+        }
+    }
 };
