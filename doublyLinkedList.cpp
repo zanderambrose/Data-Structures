87d9e435ed9a21c0ec7906026aca4b355c5f40e9
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-17 07:22:19 -0400

    Begin append method for Dll.

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -30,6 +30,24 @@ public:
         this->tail = firstNode;
         this->length = 1;
     }
+    void append(int value)
+    {
+        Node *newNode = new Node(value);
+        if (this->length == 1)
+        {
+            this->head->next = newNode;
+            newNode->previous = this->head;
+            this->tail = newNode;
+        }
+        else
+        {
+
+            this->tail->next = newNode;
+            newNode->previous = this->tail;
+            this->tail = newNode;
+        }
+        this->length++;
+    }
     void getHead()
     {
         std::cout << "Head: " << this->head->value << std::endl;
@@ -43,8 +61,25 @@ public:
         Node *current = this->head;
         while (current)
         {
-            std::cout << current->value << std::endl;
+            std::cout << current->value << ", ";
             current = current->next;
         }
+        std::cout << std::endl;
     }
 };
+
+int main()
+{
+    DoublyLinkedList *myDll = new DoublyLinkedList(10);
+
+    myDll->printList();
+
+    myDll->append(4);
+    myDll->append(9);
+    myDll->append(6);
+    myDll->append(2);
+    myDll->append(4);
+    myDll->printList();
+    myDll->getHead();
+    myDll->getTail();
+}
