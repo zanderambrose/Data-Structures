7ebb8c344a962566f10fedb45ceec92d8f8541fa
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-19 15:19:21 -0400

    Make constructor.

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -6,11 +6,25 @@ public:
     int value;
     Node *right;
     Node *left;
+
     Node(int value)
     {
         this->value = value;
         this->right = nullptr;
         this->left = nullptr;
+    }
+};
+
+class BinarySearchTree
+{
+private:
+    Node *root;
+
+public:
+    BinarySearchTree(int value)
+    {
+        Node *newNode = new Node(value);
+        this->root = newNode;
     }
 };
 
