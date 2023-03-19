30da2756a666d14e60bff53b8a50fca4dee8a212
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-19 15:32:55 -0400

    First draft insert BST node.

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -30,6 +30,45 @@ public:
     {
         this->root = nullptr;
     }
+    void insert(int value)
+    {
+        Node *newNode = new Node(value);
+        if (this->root == nullptr)
+        {
+            this->root = newNode;
+            return;
+        }
+        Node *temp = this->root;
+        while (temp)
+        {
+            if (temp->value == newNode->value)
+                return;
+            if (value < temp->value)
+            {
+                if (temp->left)
+                {
+                    temp = temp->left;
+                }
+                else
+                {
+                    temp->left = newNode;
+                    break;
+                }
+            }
+            else
+            {
+                if (temp->right)
+                {
+                    temp = temp->right;
+                }
+                else
+                {
+                    temp->right = newNode;
+                    break;
+                }
+            }
+        }
+    }
 };
 
 int main()
