450e58a13c31dc7b12ccf96c0d5b7aa1efc3bf3b
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-19 11:40:29 -0400

    WIP: queue.

--- queue.cpp
+++ queue.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
 
+class Node
+{
 
-template <typename T>
-class Queue{
-	private:
-		T item[10];	
-		T head;
-		T tail;
-
-	public:
-		void enqueue();
-		void dequeue();
-		void push();
-		void pop();
-		void empty();
-		void size();
-
-
+public:
+	int value;
+	Node *next;
+	Node(int value)
+	{
+		Node *newNode = new Node(value);
+		newNode->next = nullptr;
+	}
 };
 
+class Queue
+{
+private:
+	int length;
+	Node *front;
+	Node *back;
 
-int main(){
-	Queue<int> myQ;
+public:
+	Queue(int value)
+	{
+		Node *newNode = new Node(value);
+		this->front = newNode;
+		this->back = newNode;
+		this->length = 1;
+	}
+	void enqueue(int value)
+	{
+		Node *newNode = new Node(value);
+		this->back->next = newNode;
+		this->back = newNode;
+		this->length++;
+	}
+	int dequeue()
+	{
+		if (this->length <= 0)
+			return INT_MIN;
+		int frontValue = this->front->value;
+		Node *tempFront = this->front;
+		this->front = tempFront->next;
+		this->length--;
+		delete tempFront;
+		return frontValue;
+	}
+};
+
+int main()
+{
+	Queue myQ(1);
 	return 0;
 }
-
 
 // push, pop, size, front, back
