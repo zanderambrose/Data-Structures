63ed4c1a027af01e6fd049c7d3c8b8aad2fcd1b4
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-20 08:02:58 -0400

    Implement set and print table methods.

--- hashTable.cpp
+++ hashTable.cpp
@@ -34,11 +34,52 @@ public:
         std::cout << "Hash is: " << hash << std::endl;
         return hash;
     }
+    void set(std::string key, int value)
+    {
+        int index = this->hash(key);
+        Node *newNode = new Node(key, value);
+        if (this->dataMap[index] == nullptr)
+        {
+            this->dataMap[index] = newNode;
+        }
+        else
+        {
+            Node *temp = this->dataMap[index];
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void printTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            std::cout << i << ":" << std::endl;
+            if (dataMap[i])
+            {
+                Node *temp = dataMap[i];
+                while (temp)
+                {
+                    std::cout << "   {" << temp->key << ", " << temp->value << "}" << std::endl;
+                    temp = temp->next;
+                }
+            }
+        }
+    }
 };
 
 int main()
 {
-    HashTable hash;
-    hash.hash("father");
+    HashTable *hash = new HashTable();
+    hash->set("nails", 100);
+    hash->set("tile", 50);
+    hash->set("lumber", 80);
+
+    hash->set("bolts", 200);
+    hash->set("screws", 140);
+
+    hash->printTable();
     return 0;
 }
