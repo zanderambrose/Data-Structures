a47556ed67017255da7636fde3bd43784a130307
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-21 08:09:28 -0400

    Quick test.

--- graph.cpp
+++ graph.cpp
@@ -56,8 +56,11 @@ int main()
     Graph *myGraph = new Graph();
     myGraph->addVertex("A");
     myGraph->addVertex("B");
+    myGraph->addVertex("C");
 
     myGraph->addEdge("A", "B");
+    myGraph->addEdge("A", "C");
+    myGraph->addEdge("B", "C");
     myGraph->printGraph();
 
     myGraph->removeEdge("A", "B");
